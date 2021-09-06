4b141adc7fd890c4f5b3ffec4245addc68c0bf4e
Andrew Thawley <thawleya@staff.cbhs.school.nz>
2021-09-06 23:18:37 +1200

    Top Down Car movement

--- Assets/TopDown2DMovement/PlayerMovement2DCar.cs
+++ Assets/TopDown2DMovement/PlayerMovement2DCar.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerMovement2D : MonoBehaviour
+public class PlayerMovement2DCar : MonoBehaviour
 {
+    float drift = 0.5f;
     Vector2 movement;
     Rigidbody2D rb;
     public float speed = 5;
@@ -22,6 +23,15 @@ public class PlayerMovement2D : MonoBehaviour
     }
  
     void FixedUpdate() {
+
+        // Get Forward Velocity
+        Vector2 forwardVelocity = transform.up * Vector2.Dot(rb.velocity, transform.up);
+
+        // Get Right Velocity (drift)
+        Vector2 rightDrift = transform.right * Vector2.Dot(rb.velocity, transform.right);
+
+        rb.velocity = forwardVelocity + rightDrift * drift;
+
         rb.AddForce(transform.up * speed * movement.y * Time.deltaTime, ForceMode2D.Force);
         rb.MoveRotation(rb.rotation + turnSpeed * -movement.x * Time.deltaTime);
 
