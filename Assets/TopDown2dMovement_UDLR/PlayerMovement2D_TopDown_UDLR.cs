3178cd0058c29ef132823a66420792446588e5cd
Andrew Thawley <thawleya@staff.cbhs.school.nz>
2021-09-07 14:49:37 +1200

    Collision Detection added

--- Assets/TopDown2dMovement_UDLR/PlayerMovement2D_TopDown_UDLR.cs
+++ Assets/TopDown2dMovement_UDLR/PlayerMovement2D_TopDown_UDLR.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement2D_TopDown_UDLR : MonoBehaviour
 {
+    // Object Variables
+    Rigidbody2D rb;
+    Vector2 movement;
+    public float playerSpeed = 5;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void OnMove(InputValue iv) {
+        movement = iv.Get<Vector2>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.MovePosition(rb.position + movement * playerSpeed * Time.deltaTime);
     }
 }
