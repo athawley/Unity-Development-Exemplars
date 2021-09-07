04308fe8fb686df2dfbcf59cf9abe34e962dbc7a
Andrew Thawley <thawleya@staff.cbhs.school.nz>
2021-09-07 13:47:31 +1200

    Before coding UDLR 2D Player Movement

---
+++ Assets/TopDown2dMovement_UDLR/PlayerMovement2D_TopDown_UDLR.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement2D_TopDown_UDLR : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
