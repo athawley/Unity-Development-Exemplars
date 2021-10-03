6a71d3633eeceb6df771c2f887b7bb1c4d3e7b4e
Andrew Thawley <thawleya@staff.cbhs.school.nz>
2021-10-03 22:16:56 +1300

    Enemy Health Bar appears at player

--- Assets/WIP/VisibleEnemyManager.cs
+++ Assets/WIP/VisibleEnemyManager.cs
@@ -1,22 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VisibleEnemyManager : MonoBehaviour
 {
     public Transform target;
     public Camera cam;
+    public Canvas enemyCanvas;
+
+    public GameObject[] enemies;
+    public GameObject[] enemyText;
+
+    public GameObject enemyUIprefab;
 
     void Start()
     {
+        enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        enemyText = new GameObject[enemies.Length];
+        foreach(GameObject enemy in enemies) {
+            Debug.Log(enemy.GetInstanceID());
+        }
         //cam = GetComponent<Camera>();
+
+        // Get list of players
     }
+
+
 
     void Update()
     {
-        
+        //
+        for(int i = 0; i < enemies.Length; i++) {
+            Transform target = enemies[i].transform;
+            Vector3 screenPos = cam.WorldToScreenPoint(target.position);
+            //Debug.Log(enemy + " target is " + screenPos.x + " pixels from the left");
+            //Debug.Log(enemy + " target is " + screenPos.y + " pixels from the top");
+            
+            if(enemyText[i] == null) {
+                //enemyText[i] = new GameObject("myTextGO");
+                enemyText[i] = Instantiate(enemyUIprefab, new Vector3(0,0,0), Quaternion.identity);
+                //Text myText = enemyText[i].AddComponent<Text>();
+                //myText.text = enemies[i].ToString();
+                enemyText[i].transform.SetParent(enemyCanvas.transform);
+
+            }
+            enemyText[i].transform.position = new Vector2(screenPos.x, screenPos.y);
+            //enemyText[i].transform.position = new Vector2(0,0);
+        }
+
+        /*
         Vector3 screenPos = cam.WorldToScreenPoint(target.position);
-        Debug.Log("target is " + screenPos.x + " pixels from the left");
-        Debug.Log("target is " + screenPos.y + " pixels from the top");
+        
+        
+        if(go == null) {
+        go = Instantiate(enemy)
+        go.transform.SetParent(enemyCanvas.transform);
+        Text myText = go.AddComponent<Text>();
+        myText.text = go.ToString();
+        myText.font = new Font("Serif");
+        }
+        
+        go.transform.position = new Vector2(screenPos.x, screenPos.y);
+        */
+        
     }
 }
