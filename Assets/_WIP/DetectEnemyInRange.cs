c96cb6668d10e590033e85563e0e9a59366d786d
Andrew Thawley <thawleya@staff.cbhs.school.nz>
2021-10-12 19:40:49 +1300

    Enemy chases player until at set distance
    Code needs to be optimised.

--- Assets/_WIP/DetectEnemyInRange.cs
+++ Assets/_WIP/DetectEnemyInRange.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class DetectEnemyInRange : MonoBehaviour
 {
-    public float stopDistance = 1; // Object will stop chasing at this point
-    public float chaseDistance = 4; // Object will chase from this point.
+    public float stopDistance = 4; // Object will stop chasing at this point
+    public float chaseDistance = 8; // Object will chase from this point.
 
     public string[] detectionTags; // array of tags that are to be detected
+
+    private NavMeshAgent navAgent;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        navAgent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
@@ -29,6 +32,17 @@ public class DetectEnemyInRange : MonoBehaviour
             if(hitCollider.CompareTag("Enemy")) {
                 //Debug.Log("In range: " + hitCollider.gameObject.ToString());
                 transform.LookAt(hitCollider.transform, Vector3.up); // Face object inside the detectable area.
+
+                // Move to the player
+                
+                //Debug.Log(Vector3.Distance(hitCollider.transform.position, transform.position));
+                if(Vector3.Distance(hitCollider.transform.position, transform.position) < stopDistance) {
+                   
+                   navAgent.isStopped = true;
+                   navAgent.ResetPath();
+                } else {
+                    navAgent.SetDestination(hitCollider.transform.position);
+                }
             }
         }
     }
