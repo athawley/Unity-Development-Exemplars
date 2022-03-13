5c4f4e50e6adab973e59d3bb1258d07147dfc9ed
Andrew Thawley <58050814+athawley@users.noreply.github.com>
2022-03-13 14:13:30 +1300

    Dual Character Control with one Player working

--- Assets/_WIP/KeepLocalPlayersInScene/PlayerJoinCameraManager.cs
+++ Assets/_WIP/KeepLocalPlayersInScene/PlayerJoinCameraManager.cs
@@ -7,12 +7,6 @@ public class PlayerJoinCameraManager : MonoBehaviour
 {
     public CinemachineTargetGroup targetGroup;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        targetGroup = GetComponent<CinemachineTargetGroup>();
-    }
-
     void OnPlayerJoined(PlayerInput pi) {
         targetGroup.AddMember(pi.gameObject.transform, 3f, 5f);
     }
