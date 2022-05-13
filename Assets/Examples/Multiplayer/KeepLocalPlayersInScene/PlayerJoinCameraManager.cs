8808f3854d96992dcc034f69440784bc907e2169
Andrew Thawley <58050814+athawley@users.noreply.github.com>
2022-05-13 21:28:45 +1200

    moving incomplete work

---
+++ Assets/Examples/Multiplayer/KeepLocalPlayersInScene/PlayerJoinCameraManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using UnityEngine.InputSystem;
+public class PlayerJoinCameraManager : MonoBehaviour
+{
+    public CinemachineTargetGroup targetGroup;
+
+    void OnPlayerJoined(PlayerInput pi) {
+        targetGroup.AddMember(pi.gameObject.transform, 3f, 5f);
+        
+    }
+}
