07307dc85ccce6ed9e25cb5c2c0149226d7abb3d
Andrew Thawley <58050814+athawley@users.noreply.github.com>
2022-06-01 11:05:34 +1200

    Weapon swinging

---
+++ Assets/_WIP/MeleeWeapon/MeleeWeaponManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MeleeWeaponManager : MonoBehaviour
+{
+    bool _firePressed = false;
+    public Animator weaponAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnFire() {
+        Debug.Log("Attack Pressed");
+        weaponAnimator.SetBool("AttackPressed", true); 
+    }
+}
